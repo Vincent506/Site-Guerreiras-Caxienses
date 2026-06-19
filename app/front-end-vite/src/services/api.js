e01dd9a092f35680567df3c6c9fb146d0dd9969f
Vincent506 <vicenteneto0806@gmail.com>
2026-06-19 08:48:30 -0300

    <Adição> 2 novas conexões do front-end com o back-end(Autenticação de Registro e Autenticação de Login)

--- app/front-end-vite/src/services/api.js
+++ app/front-end-vite/src/services/api.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+export default axios.create({
+  baseURL: "http//localhost:3000"
+})
+
+await api.post("/app/backend/src/models/User.js",{
+  nome,
+  email,
+  senha
+});
+
+await api.post("/app/backend/src/controllers/authController.js",{
+  email,
+  senha
+})
+
+
 const api = axios.create({
   baseURL: "http://localhost:5173"
 });
